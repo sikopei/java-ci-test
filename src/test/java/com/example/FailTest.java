b87cc04ca842d7a9670428d9a56511a8f6f763af
sikopei <celebrity_1999@yahoo.co.jp>
2025-09-02 14:57:26 +0900

    Create FailTest.java

---
+++ src/test/java/com/example/FailTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FailTest {
+
+    @Test
+    void thisWillFail() {
+        int result = 2 + 2;
+        // わざと間違えた期待値
+        assertEquals(5, result, "2+2 should NOT equal 5");
+    }
+}
