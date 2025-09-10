a81d81c3a6d6c38dfea2d182cf1057ad6527b057
sikopei <celebrity_1999@yahoo.co.jp>
2025-09-10 17:05:25 +0900

    Update seiko1Test.java

--- src/test/java/com/example/seiko1Test.java
+++ src/test/java/com/example/seiko1Test.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SuccessTest {
+public class seiko1Test {
 
     @Test
     void testAddition() {
